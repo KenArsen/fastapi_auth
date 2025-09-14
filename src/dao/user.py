39a3c0983ed7d3f27e6209578ef675d482e9b04a
KenArsen <kenzhegulov.kgz@gmail.com>
2025-09-14 19:04:55 +0600

    Добавил аннотация

--- src/dao/user.py
+++ src/dao/user.py
@@ -1,10 +1,11 @@
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from src.models.user import Account
 from src.dao.base import BaseDAO
 
 
 class AccountDAO(BaseDAO):
-    def __init__(self, session):
+    def __init__(self, session: AsyncSession):
         super().__init__(Account, session)
 
     async def get_by_email(self, email: str) -> Account | None:
